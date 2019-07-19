e83e87b492218061278f01bd0f70fd14d357e79d
Harika <harika.g.gullapalli@gmail.com>
2019-07-19 16:04:37 -0500

    Counter changes for mines flag and timer

--- src/ms/view/Counter.java
+++ src/ms/view/Counter.java
@@ -1,39 +1,123 @@
 package ms.view;
+
 /**
  * This is a reusable UI component as a counter 
  * showing the number of mines and time.
  *
  */
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
+public class Counter extends JPanel {
+	private static final long serialVersionUID = 1L;
 
+	private int value;
+	private int initValue;
+	private int upper;
+	private int lower;
+	private String format;
+	private JFrame frame;
+	private JLabel label;
+	private Timer timer;
+	private static Font font;
 
-public class Counter extends JPanel{
-	int value = 0; 
-	public Counter (int initValue, int upper, int lower) {
-		
+	public Counter(int initValue, int upper, int lower) {
+		this.initValue = initValue;
+		this.upper = upper;
+		this.lower = lower;
 	}
-	
+
 	public void setValue(int value) {
-		
+		this.value = value;
 	}
-	
+
 	public int getValue() {
-		return value; 
+		return value;
 	}
-	
+
 	public void increase() {
-		
+		if (value < upper) {
+			value++;
+			setLabelText(value);
+		}
 	}
-	
+
 	public void decrease() {
-		
-		
+		if (value > lower) {
+			value--;
+			setLabelText(value);
+		}
 	}
+
+	public void reset() {
+		setLabelText(initValue);
+	}
+
+	public void createTimerAction(int delay) {
+		
+		timer = new Timer(delay, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				increase();
+			}
+		});
+		timer.setInitialDelay(0);
+		timer.start();
+	}
+
+	private void setLabelText(int val) {
+		label.setText(String.format(format, val));
+	}
+
+	public void draw() {
+		format = "%0" + String.valueOf(upper).length() + "d";
+		frame = new JFrame();
+		label = new JLabel(String.format(format, initValue));
+		label.setForeground(Color.red);
+		label.setFont(font);
+
+		JPanel panel = new JPanel();
+		panel.setBackground(Color.black);
+		panel.add(label);
+		frame.add(panel);
+		// set the size of frame
+		frame.setSize(50, 50);
+		frame.pack();
+		frame.setVisible(true);
+		setValue(initValue);
+
+	}
+
+	private static void loadFont() {
+		// Load font from file
+		File file = new File("./fonts/DJB Get Digital.ttf");  
+		try {
+			font = Font.createFont(Font.TRUETYPE_FONT, file);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		font = font.deriveFont(Font.BOLD, 30);
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		ge.registerFont(font);
+	}
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		// test it in a JFrame 
-		// 
+		loadFont();
+		/** For flag counter **/
+		Counter counter = new Counter(15, 15, 0);
+		counter.draw();
+		/** For timer **/
+		Counter timer = new Counter(0, 999, 0);
+		timer.draw();
+		timer.createTimerAction(999);
 	}
 
 }
