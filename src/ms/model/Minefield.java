59afbd6c21f3d980525cf41335f36ea251b40121
RJPIII <robertpreston@austin.rr.com>
2019-08-05 16:34:43 -0500

    Updated Javadocs for Model and Minefield files

--- src/ms/model/Minefield.java
+++ src/ms/model/Minefield.java
@@ -313,6 +313,7 @@ public class Minefield implements Model{
 	 * @param y				The column value of the cell.
 	 * @param visibility	The desired visibility value at index [x, y]; -1 if index [x, y]
 	 * 						does not exist.
+	 * @return				0 for success; 1 for failure
 	 */	
 	public int setVisibleValue(int x, int y, int visibility){
 		try{
@@ -358,8 +359,9 @@ public class Minefield implements Model{
 	 * 
 	 * @param x				The row value of the cell.
 	 * @param y				The column value of the cell.
-	 * @param visibility	The desired mine value at index [x, y]; -1 if index [x, y] does 
+	 * @param mine_value	The desired mine value at index [x, y]; -1 if index [x, y] does 
 	 * 						not exist.
+	 * @return				0 for success; 1 for failure
 	 */	
 	public int setMineValue(int x, int y, int mine_value){
 		try{
@@ -381,9 +383,9 @@ public class Minefield implements Model{
 	 * Reconstructs the fields using the given number of rows, columns and mines. The fields, 
 	 * status and indicators are then set to their default values.
 	 * 
-	 * @param rows
-	 * @param columns
-	 * @param numMines
+	 * @param rows			The desired new number of rows
+	 * @param columns		The desired new number of columns
+	 * @param numMines		The desired new number of mines
 	 */
 	public void redoField(int rows, int columns, int numMines) {
 		try {
